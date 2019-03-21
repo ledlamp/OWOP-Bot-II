d77fa442182f0fce205ec250f02f5b165e366160
Lamp <ledlamp89@gmail.com>
2019-03-21 13:04:13 -0700

    Add captcha bypass to archiver

--- archive.js
+++ archive.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 var Discord = require('discord.js');
 var puppeteer = require("puppeteer");
-var {archive_webhook} = require("./credentials");
-archive_webhook = new Discord.WebhookClient(archive_webhook.id, archive_webhook.token);
+var credentials = require("./credentials");
+var archive_webhook = new Discord.WebhookClient(credentials.archive_webhook.id, credentials.archive_webhook.token);
 
 (async function archive() {
     console.log("Archive started");
@@ -10,15 +10,25 @@ archive_webhook = new Discord.WebhookClient(archive_webhook.id, archive_webhook.
         var browser = await puppeteer.launch({args:['--no-sandbox']});
         var page = await browser.newPage();
         await page.setViewport({width:2048, height:2048});
+        console.log("Page opened");
         await page.goto("https://ourworldofpixels.com");
-        await page.evaluate(function () { OWOP.camera.zoom = 1; });
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        await page.evaluate(function () {
+        console.log("OWOP Loaded");
+        await page.evaluate(async function (captcha_password) {
+            OWOP.camera.zoom = 1;
+            localStorage.owopOptions = '{"defaultZoom": 1}' //todo "noUi":true
+            localStorage.owopcaptcha = captcha_password;
             for (let butt of document.getElementsByTagName('button')) {
                 if (butt.innerText == 'OK') {butt.click();break}
             }
-        });
-        await new Promise(resolve => setTimeout(resolve, 60000));
+            await new Promise(resolve => {
+                //OWOP.once(OWOP.events.allChunksLoaded, () => { // todo
+                    resolve();
+                //});
+            });
+        }, credentials.captcha_password);
+        console.log("Waiting a minute...");
+        await new Promise(resolve => setTimeout(resolve, 60000)); //todo reduce if OWOP.events.allChunksLoaded works
+        console.log("Saving screenshot");
         let screenshot = await page.screenshot({ type: 'png' });
         let filename = `Screenshot of ourworldofpixels.com/main @ ${new Date().toISOString()}.png`;
         let attachment = new Discord.Attachment(screenshot, filename);
