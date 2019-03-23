40c2791587f97defa067c252f49f7d2b3c49ce68
Lamp <ledlamp89@gmail.com>
2019-03-23 10:55:01 -0700

    try..finally not working as desired as somewhere in page.evaluate() a promise throws unhandled rejection if page crashes; use "unhandledRejection" event instead

--- archive.js
+++ archive.js
@@ -6,35 +6,37 @@ var archive_webhook = new Discord.WebhookClient(config.archive_webhook.id, confi
 
 (async function archive() {
     console.log("Archive started");
-    try {
-        var browser = await puppeteer.launch({args:['--no-sandbox']});
-        var page = await browser.newPage();
-        await page.setViewport({width:2048, height:2048});
-        console.log("Page opened");
-        await page.goto("https://ourworldofpixels.com");
-        console.log("OWOP Loaded");
-        await page.evaluate(async function (captcha_password) {
-            OWOP.camera.zoom = 1;
-            OWOP.options.noUi = true;
-            localStorage.owopOptions = '{"defaultZoom": 1, "noUi": true}';
-            localStorage.owopcaptcha = captcha_password;
-            for (let butt of document.getElementsByTagName('button')) {
-                if (butt.innerText == 'OK') {butt.click();break}
-            }
-            await new Promise(resolve => {
-                OWOP.once(OWOP.events.allChunksLoaded, () => {
-                    resolve();
-                });
-                setTimeout(resolve, 60000);
+    var browser = await puppeteer.launch({args:['--no-sandbox']});
+    var page = await browser.newPage();
+    await page.setViewport({width:2048, height:2048});
+    console.log("Page opened");
+    await page.goto("https://ourworldofpixels.com");
+    console.log("OWOP Loaded");
+    await page.evaluate(async function (captcha_password) {
+        OWOP.camera.zoom = 1;
+        OWOP.options.noUi = true;
+        localStorage.owopOptions = '{"defaultZoom": 1, "noUi": true}';
+        localStorage.owopcaptcha = captcha_password;
+        for (let butt of document.getElementsByTagName('button')) {
+            if (butt.innerText == 'OK') {butt.click();break}
+        }
+        await new Promise(resolve => {
+            OWOP.once(OWOP.events.allChunksLoaded, () => {
+                resolve();
             });
-        }, config.captcha_password);
-        console.log("Saving screenshot");
-        let screenshot = await page.screenshot({ type: 'png' });
-        let filename = `Screenshot of ourworldofpixels.com/main @ ${new Date().toISOString()}.png`;
-        let attachment = new Discord.Attachment(screenshot, filename);
-        await archive_webhook.send(attachment);
-        console.log("Archive finished");
-    } finally {
-        process.exit();
-    }
+            setTimeout(resolve, 60000);
+        });
+    }, config.captcha_password);
+    console.log("Saving screenshot");
+    let screenshot = await page.screenshot({ type: 'png' });
+    let filename = `OWOP archive ${new Date().toISOString()}.png`;
+    let attachment = new Discord.Attachment(screenshot, filename);
+    await archive_webhook.send(attachment);
+    console.log("Archive finished");
+    await browser.close();
 })();
+
+process.on("unhandledRejection", error => {
+    console.error(error);
+    process.exit(1);
+});
