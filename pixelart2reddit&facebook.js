4eb2630424dc9ca0cc6b8f9a0f4d79cd6896b1a8
Lamp <ledlamp89@gmail.com>
2019-04-20 15:47:58 -0700

    exclude b! commands from sending to reddit

--- pixelart2reddit&facebook.js
+++ pixelart2reddit&facebook.js
@@ -9,7 +9,7 @@ module.exports = function(discordBot) {
         password: config.reddit.user_pass
     });
     discordBot.on("message", message => {
-        if (message.channel.id == "350437018989363211" && message.attachments.first() && message.attachments.first().width) {
+        if (message.channel.id == "350437018989363211" && !message.content.startsWith("b!") && message.attachments.first() && message.attachments.first().width) {
             console.log("submitting pixel art to reddit:", message.attachments.first().url);
             r.getSubreddit("OurWorldOfPixels").submitLink({
                 "title": `[OWOP Bot|#pixel-art] from ${message.author.tag}${message.content ? ": " + message.cleanContent : ""}`.substr(0,300),
