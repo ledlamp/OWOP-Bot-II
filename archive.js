414330cd8ae056393965e686e589dbf4956aea4e
Lamp <ledlamp89@gmail.com>
2019-06-24 12:14:53 -0700

    fix web console logging

--- archive.js
+++ archive.js
@@ -5,11 +5,13 @@ var config = require("./config");
 var archive_webhook = new Discord.WebhookClient(config.archive_webhook.id, config.archive_webhook.token);
 
 (async function archive() {
-    console.log("Archive started");
+    console.log("Archive started @", Date());
     var browser = await puppeteer.launch({args:['--no-sandbox']});
     var page = await browser.newPage();
     page.on("console", function(){
-        console.log.apply(console, Array.from(arguments).unshift("[Web Console]"));
+        let args = Array.from(arguments);
+        args.unshift("[Web Console]");
+        console.log.apply(console, args);
     });
     await page.setViewport({width:2048, height:2048});
     console.log("Page opened");
